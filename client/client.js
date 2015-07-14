b3f9ec8948ae78b818f2c2fdbd44e43f1fe0f8b8
Daron <daronlin303@gmail.com>
2015-07-13 22:46:09 -0400

    stub out custom login and register form

--- client/client.js
+++ client/client.js
@@ -5,6 +5,20 @@ Template.light_ui.helpers({
 
 });
 
+Template.register.events({
+  "click #register-submit": function(event) {
+    event.preventDefault();
+    console.log("register submitted");
+  }
+});
+
+Template.login.events({
+  "click #login-submit": function(event) {
+    event.preventDefault(); 
+    console.log("login clicked");
+  }
+});
+
 Template.light_ui.events({
   "click .light_switch" : function() {
       Meteor.call("toggleLight", Meteor.userId());
